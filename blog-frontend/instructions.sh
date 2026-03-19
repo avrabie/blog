d45611994201e099df1dc15d2189dd842ac6f727
moldo <44651279+avrabie@users.noreply.github.com>
2026-03-19 16:21:04 +0100

    updating instructions

--- blog-frontend/instructions.sh
+++ blog-frontend/instructions.sh
@@ -3,5 +3,5 @@
 docker build --platform linux/amd64 -t gluonstream/blog-fe:latest .
 docker push gluonstream/blog-fe:latest
 #kind load docker-image gluonstream/blog-fe:latest --name blog.s4v3
-kubectl apply -k k8s
-kubectl rollout restart deployment/blog-fe -n blog-namespace
+#kubectl apply -k k8s
+#kubectl rollout restart deployment/blog-fe -n blog-namespace
